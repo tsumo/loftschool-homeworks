1310eceba551eac2282916c3751a9f616d4131a7
tsumo <tsumo@users.noreply.github.com>
2019-05-02 15:51:03 +0300

    homework 7 done

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -7,45 +7,84 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 // Обратите внимание на `orders.test.js`.
 // Он поможет понять, какие значения должен возвращать редьюсер.
 
-const conveyor = ["clients", "conveyor_1", "conveyor_2", "conveyor_3", "conveyor_4"]
-
-export const orders = [];
-
-export default (state = orders, action) => {
+export default (state = [], action) => {
   switch (action.type) {
     case CREATE_NEW_ORDER:
       const { id, recipe } = action.payload;
       return [
         ...state,
         {
-          id, recipe,
+          id,
+          recipe,
           ingredients: [],
-          position: "clients"
+          position: 'clients'
         }
-      ]
+      ];
     case MOVE_ORDER_NEXT:
-      return [
-        ...state,
-        changePosition(state, action, 1)
-      ]
+      return changePosition(state, action.payload, 1);
     case MOVE_ORDER_BACK:
-      return [
-        ...state,
-        changePosition(state, action, -1)
-      ]
+      return changePosition(state, action.payload, -1);
+    case ADD_INGREDIENT:
+      const { from, ingredient } = action.payload;
+      const orders = getOrdersFor({ orders: state }, from);
+      if (orders.length === 0) {
+        return state;
+      }
+      return state.map(order => {
+        return order.id === orders[0].id
+          ? addIngredient(order, ingredient)
+          : order;
+      });
     default:
       return state;
   }
 };
 
-export const getOrdersFor = (state, position) =>
-  state.orders.filter(order => order.position === position);
+const conveyor = [
+  'clients',
+  'conveyor_1',
+  'conveyor_2',
+  'conveyor_3',
+  'conveyor_4',
+  'finish'
+];
 
+const checkIngredients = order => {
+  return order.recipe.every(ingredient => {
+    return order.ingredients.includes(ingredient);
+  });
+};
 
-const changePosition = (state, action, vector) => {
+const changePosition = (state, id, delta) => {
   return state.map(order => {
-    if (order.id == action.payload) {
-      order.position = conveyor[conveyor.indexOf(order.position) + vector]
+    const newPosition = conveyor[conveyor.indexOf(order.position) + delta];
+    const isDone = checkIngredients(order);
+    let allowChange = false;
+    if (order.id === id) {
+      if (newPosition === 'finish') {
+        if (isDone) {
+          allowChange = true;
+        }
+      } else if (newPosition === 'clients') {
+        allowChange = false;
+      } else {
+        allowChange = true;
+      }
     }
-  })
+    return allowChange ? { ...order, position: newPosition } : order;
+  });
+};
+
+const addIngredient = (order, ingredient) => {
+  if (
+    order.recipe.includes(ingredient) &&
+    !order.ingredients.includes(ingredient)
+  ) {
+    return { ...order, ingredients: [...order.ingredients, ingredient] };
+  }
+  return order;
+};
+
+export const getOrdersFor = (state, position) => {
+  return state.orders.filter(order => order.position === position);
 };
